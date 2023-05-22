ac71f0f5687af879f5b7f9a96b02105d03deaa3a
marcoskaiser <117455066+marcoskaiser@users.noreply.github.com>
2023-05-22 10:49:00 -0300

    Co-authored-by: Bernardo Zamin <Bernardo-Zamin@users.noreply.github.com>

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -5,7 +5,7 @@ import pucrs.myflight.modelo.Aeroporto;
 import pucrs.myflight.modelo.CiaAerea;
 import pucrs.myflight.modelo.Geo;
 import pucrs.myflight.modelo.GerenciadorAeronaves;
-import pucrs.myflight.modelo.LeituraDados;
+import pucrs.myflight.modelo.LeitorDadosAirPort;
 import pucrs.myflight.modelo.LeituraDadosAirlines;
 import pucrs.myflight.modelo.Rota;
 import pucrs.myflight.modelo.Voo;
@@ -26,57 +26,60 @@ public class App {
 		
 
 		// Duration agora = Duration
-		GerenciadorAeronaves gerenciaAeronave = new GerenciadorAeronaves();
+		// GerenciadorAeronaves gerenciaAeronave = new GerenciadorAeronaves();
 		
-		Geo latlong1 = new Geo(-29.9939,-51.1711);
-		Geo latlong2 = new Geo(-23.4356,-46.4731);
+		// Geo latlong1 = new Geo(-29.9939,-51.1711);
+		// Geo latlong2 = new Geo(-23.4356,-46.4731);
 		
-		Aeronave aeronaveA = new Aeronave("733", "Boeing 737-300");
-		Aeronave aeronaveB = new Aeronave("12G", "Airbus Industrie A380");
-		Aeronave aeronaveC = new Aeronave("380", "Boeing 123-456");
+		// Aeronave aeronaveA = new Aeronave("733", "Boeing 737-300");
+		// Aeronave aeronaveB = new Aeronave("12G", "Airbus Industrie A380");
+		// Aeronave aeronaveC = new Aeronave("380", "Boeing 123-456");
 		
-		CiaAerea ciaAereaA = new CiaAerea("JJ", "LATAM Linhas Aéreas");
-		CiaAerea ciaAereaB  = new CiaAerea("G3", "Gol Linhas Aéreas SA");
-		CiaAerea ciaAereaC = new CiaAerea("TP", "TAP Portugal");
+		// CiaAerea ciaAereaA = new CiaAerea("JJ", "LATAM Linhas Aéreas");
+		// CiaAerea ciaAereaB  = new CiaAerea("G3", "Gol Linhas Aéreas SA");
+		// CiaAerea ciaAereaC = new CiaAerea("TP", "TAP Portugal");
 		
-		Aeroporto aeroportoA = new Aeroporto("POA", "Salgado Filho Intl Apt", latlong1);
-		Aeroporto aeroportoB = new Aeroporto("GRU", "São Paulo Guarulhos Intl Apt", latlong2);
+		// Aeroporto aeroportoA = new Aeroporto("POA", "Salgado Filho Intl Apt", latlong1);
+		// Aeroporto aeroportoB = new Aeroporto("GRU", "São Paulo Guarulhos Intl Apt", latlong2);
 
-		System.out.println(latlong1.distancia(latlong2));
+		// System.out.println(latlong1.distancia(latlong2));
 
-		System.out.println(Geo.distancia(latlong1, latlong2));
+		// System.out.println(Geo.distancia(latlong1, latlong2));
 
-		System.out.println();
+		// System.out.println();
 
-		Rota rota1 = new Rota(ciaAereaA,aeroportoA,aeroportoB,aeronaveA);
-		Rota rota2 = new Rota(ciaAereaB, aeroportoB, aeroportoA, aeronaveC);
+		// Rota rota1 = new Rota(ciaAereaA,aeroportoA,aeroportoB,aeronaveA);
+		// Rota rota2 = new Rota(ciaAereaB, aeroportoB, aeroportoA, aeronaveC);
 
-		LocalDateTime datahora = LocalDateTime.of(2003, 5, 27, 15, 30);
-		Duration duracao = Duration.ofMinutes(120); // 2 horas
+		// LocalDateTime datahora = LocalDateTime.of(2003, 5, 27, 15, 30);
+		// Duration duracao = Duration.ofMinutes(120); // 2 horas
 
-		Voo voo1 = new Voo(rota2, duracao);
-		Voo voo2 = new Voo(rota2, datahora, duracao);
-		System.out.println(voo1.toString());
-		System.out.println(voo2.toString());
+		// Voo voo1 = new Voo(rota2, duracao);
+		// Voo voo2 = new Voo(rota2, datahora, duracao);
+		// System.out.println(voo1.toString());
+		// System.out.println(voo2.toString());
 
 
-		Voo v1 = new Voo(rota2, datahora, duracao); 
+		// Voo v1 = new Voo(rota2, datahora, duracao); 
 		
-		gerenciaAeronave.adicionar(aeronaveA);
-		gerenciaAeronave.adicionar(aeronaveB);
-		gerenciaAeronave.adicionar(aeronaveC);
+		// gerenciaAeronave.adicionar(aeronaveA);
+		// gerenciaAeronave.adicionar(aeronaveB);
+		// gerenciaAeronave.adicionar(aeronaveC);
 		
-		System.out.print(gerenciaAeronave.listarTodas().toString());
+		// System.out.print(gerenciaAeronave.listarTodas().toString());
 
-		System.out.println("Total de empresas: "+CiaAerea.getTotalCias());
-		System.out.println(a.toString());
-		System.out.println(b.toString());
-		System.out.println(c.toString());
+		// System.out.println("Total de empresas: "+CiaAerea.getTotalCias());
+		// System.out.println(a.toString());
+		// System.out.println(b.toString());
+		// System.out.println(c.toString());
 		
-		LeituraDadosAirlines a = new LeituraDadosAirlines();
-		a.leArquivo("airlines.dat");	//apenas para este arquivo por enquanto..
-		a.filtraCodigo("D7");
+		// LeituraDadosAirlines a = new LeituraDadosAirlines();
+		// a.leArquivo("airlines.dat");	//apenas para este arquivo por enquanto..
+		// a.filtraCodigo("D7");
 
+		LeitorDadosAirPort air = new LeitorDadosAirPort();
+		air.leArquivo("airports.dat");
+		air.filtrarCodigo("airport", "AAA"); 
 	}
 
 	
---
+++ src/pucrs/myflight/modelo/LeitorDadosAirPort.java
@@ -0,0 +1,62 @@
+package pucrs.myflight.modelo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeitorDadosAirPort {
+    private ArrayList<String> airport;
+
+    public LeitorDadosAirPort() {
+        airport = new ArrayList<String>();
+    }
+
+    public void leArquivo(String filename) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))){
+            String linha;
+            while ((linha = bufferedReader.readLine()) != null) {
+                String[] campos = linha.split(";");
+
+                if (campos.length >= 5) {
+                    String codigo = campos[0];
+                    double latitude = Double.parseDouble(campos[1]);
+                    double longitude = Double.parseDouble(campos[2]);
+                    String nome = campos[3];
+                    String pais = campos[4];
+
+                    System.out.println("Código: " + codigo);
+                    System.out.println("Latitude: " + latitude);
+                    System.out.println("Longitude: " + longitude);
+                    System.out.println("Nome: " + nome);
+                    System.out.println("País: " + pais);
+                    System.out.println();
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+    }
+
+        public static void filtrarPorCodigo(List<String> codigos, String codigoFiltro) {
+        List<String> codigosFiltrados = new ArrayList<>();
+        
+        for (String codigo : codigos) {
+            if (codigo.equals(codigoFiltro)) {
+                codigosFiltrados.add(codigo);
+            }
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return airport.toString();
+    }
+
+    public void filtrarCodigo(String string, String string2) {
+    }
+}
